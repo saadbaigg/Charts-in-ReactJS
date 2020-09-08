9c4790def898914148eeca887fdbe377b16701fc
Saad Baig <saadbaig7777@gmail.com>
2020-09-08 11:10:35 +0500

    updated recharts

--- src/components/Recharts/Recharts.jsx
+++ src/components/Recharts/Recharts.jsx
@@ -1,30 +1,28 @@
-import React, {} from 'react'
+import React from 'react'
 import styles from './Recharts.module.css'
 import { data } from '../../data'
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts'
-import { renderCustomAxisTick } from './CustomAxisTick'
-
 
 const Recharts = () => {
 
     return (
         <div className={styles.container}>
             <h1>Recharts</h1>
+            <p>Line Chart</p>
             <div className={styles.chartContainer}>
                 <LineChart 
                     width={1200} 
                     height={500} 
                     data={data}
                 >
-                    <Line type="monotone" dataKey="uv" stroke="red" />
-                    <Line type="monotone" dataKey="pv" stroke="green" />
-                    <Line type="monotone" dataKey="amt" stroke="blue" />
                     <CartesianGrid stroke="#ccc" />
                     <XAxis 
                         dataKey="name" 
-                        // tick={renderCustomAxisTick} 
                     />
                     <YAxis />
+                    <Line type="monotone" dataKey="uv" stroke="red" />
+                    <Line type="monotone" dataKey="pv" stroke="green" />
+                    <Line type="monotone" dataKey="amt" stroke="blue" />
                 </LineChart>
             </div>
         </div>
--- src/data.js
+++ src/data.js
@@ -1,15 +1,23 @@
 export const data = [
-    { name: 1950, uv: 1, pv: 1, amt: 1 },
-    { name: 1970, uv: 1, pv: 3, amt: 4 },
-    { name: 1990, uv: 2, pv: 2, amt: 5 },
-    { name: 2000, uv: 4, pv: 3, amt: 6 },
-    { name: 2005, uv: 6, pv: 5, amt: 6 },
-    { name: 2010, uv: 6, pv: 3, amt: 6 },
-    { name: 2020, uv: 7, pv: 7, amt: 7 },
+    {
+        name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    },
+    {
+        name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    },
+    {
+        name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    },
+    {
+        name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    },
+    {
+        name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    },
+    {
+        name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    },
+    {
+        name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    },
 ];
-
-export const fetchedData = async () => {
-    const res = await fetch('https://data.opendatasoft.com/api/records/1.0/search/?dataset=world-population%40kapsarc&rows=6')
-    const data = await res.json()
-    return await data
-}
